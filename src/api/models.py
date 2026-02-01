54e15ba3ff34dba99d49ecb762ac2c4e2f23bd1e
afo32 <63719917+afo32@users.noreply.github.com>
2026-02-01 18:31:06 +0000

    ft-2-crud-cliente done

--- src/api/models.py
+++ src/api/models.py
@@ -4,16 +4,18 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+class User(db.Model): 
+    id: Mapped[int] = mapped_column(primary_key=True) 
+    name: Mapped[str] = mapped_column(String(120), nullable=False) 
+    lastname: Mapped[str] = mapped_column(String(120), nullable=False) 
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False) 
+    password: Mapped[str] = mapped_column(nullable=False) 
+    is_active: Mapped[bool] = mapped_column(Boolean(), default=True, nullable=False) 
+    
+    def serialize(self): 
+        return { 
+            "id": self.id, 
+            "name": self.name, 
+            "lastname": self.lastname, 
+            "email": self.email, 
         }
