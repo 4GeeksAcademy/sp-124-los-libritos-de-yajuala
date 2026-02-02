d3d3982d7d5301e4d02b56a53fb9a4649cc99554
yahyayatusabe <60793191+yahyayatusabe@users.noreply.github.com>
2026-02-02 11:16:33 +0000

    migrations and models/routes do it of Provider

--- src/api/models.py
+++ src/api/models.py
@@ -4,18 +4,70 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
-class User(db.Model): 
-    id: Mapped[int] = mapped_column(primary_key=True) 
-    name: Mapped[str] = mapped_column(String(120), nullable=False) 
-    lastname: Mapped[str] = mapped_column(String(120), nullable=False) 
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False) 
-    password: Mapped[str] = mapped_column(nullable=False) 
-    is_active: Mapped[bool] = mapped_column(Boolean(), default=True, nullable=False) 
-    
-    def serialize(self): 
-        return { 
-            "id": self.id, 
-            "name": self.name, 
-            "lastname": self.lastname, 
-            "email": self.email, 
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    lastname: Mapped[str] = mapped_column(String(120), nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
+    is_active: Mapped[bool] = mapped_column(
+        Boolean(), default=True, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "lastname": self.lastname,
+            "email": self.email,
+        }
+
+
+class Provider(db.Model):
+    __tablename__ = "providers"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    nombre = db.Column(
+        db.String(150),
+        nullable=False,
+        index=True
+    )
+
+    email = db.Column(
+        db.String(150),
+        unique=True,
+        nullable=True,
+        index=True
+    )
+
+    telefono = db.Column(
+        db.String(20),
+        nullable=True
+    )
+
+    password = db.Column(
+        db.String(300),
+        nullable=False
+    )
+
+    documento = db.Column(
+        db.String(20),
+        unique=True,
+        nullable=False,
+        index=True
+    )
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "email": self.email,
+            "telefono": self.telefono,
+            "password": self.password,
+            "documento": self.documento
         }
