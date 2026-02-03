6561f05073d27045f6d60f2d288526b40559a216
yahyayatusabe <60793191+yahyayatusabe@users.noreply.github.com>
2026-02-03 21:34:40 +0000

    Navbar merged

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,12 +1,16 @@
 import { Link, useLocation } from "react-router-dom";
 
 export const Navbar = () => {
-  const location = useLocation();
+	const location = useLocation();
 
-<<<<<<< HEAD
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
+				{!isClientsPage && (
+					<Link to="/clients" className="btn btn-success btn-lg m-2">
+						Clientes
+					</Link>
+				)}
 				<Link to="/">
 					<span className="navbar-brand mb-0 h1">Back to Home</span>
 				</Link>
@@ -22,21 +26,3 @@ export const Navbar = () => {
 		</nav>
 	);
 };
-=======
-  const isClientsPage = location.pathname.startsWith("/clients");
-
-  return (
-    <nav className="navbar">
-      <div className="text-center mt-5">
-
-        {!isClientsPage && (
-          <Link to="/clients" className="btn btn-success btn-lg m-2">
-            Clientes
-          </Link>
-        )}
-
-      </div>
-    </nav>
-  );
-};
->>>>>>> develop
