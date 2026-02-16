d78a91614b36fdab3b523d7d7d9eecf3cc185324
todolayla-git <todolayla@gmail.com>
2026-02-16 11:45:18 +0000

    commit

--- src/front/store.js
+++ src/front/store.js
@@ -3,10 +3,11 @@ export const initialStore = () => {
     message: null,
     todos: [],
     user: JSON.parse(localStorage.getItem("user")) || null,
-    token: localStorage.getItem("token") || null
+    token: localStorage.getItem("token") || null,
+    // CCambiado añadimos activeCart 
+    activeCart: null
   };
 };
-
 
 export default function storeReducer(store, action = {}) {
   switch (action.type) {
@@ -36,13 +37,17 @@ export default function storeReducer(store, action = {}) {
         token: action.payload
       };
 
+    // Cambiado para soportar ambos action types 
+    case "SET_ACTIVE_CART":
     case "set_active_cart":
       return {
         ...store,
         activeCart: action.payload
       };
 
+    // Cambiado para no romper la app si llega una action desconocida
     default:
-      throw Error("Unknown action.");
+      console.warn("Unknown action:", action);
+      return store;
   }
 }
