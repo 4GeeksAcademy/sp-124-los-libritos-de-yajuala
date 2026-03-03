b79fa534ab80737277f35ff2ea63f074852f2b63
yahyayatusabe <60793191+yahyayatusabe@users.noreply.github.com>
2026-03-03 17:23:48 +0100

    Fix path for FLASK_APP in render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-
 npm install --legacy-peer-deps
 npm run build
 pip install -r requirements.txt
-cd src && FLASK_APP=app.py flask db upgrade
+FLASK_APP=src/app.py flask db upgrade
