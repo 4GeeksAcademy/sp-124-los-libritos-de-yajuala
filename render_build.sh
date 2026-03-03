e2f48fc9584a34af3ee7fdf92eb7b4a3c135c6e9
yahyayatusabe <60793191+yahyayatusabe@users.noreply.github.com>
2026-03-03 16:58:53 +0100

    Update database upgrade command in render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
+
 npm install --legacy-peer-deps
 npm run build
 pip install -r requirements.txt
-flask db upgrade
+cd src && FLASK_APP=app.py flask db upgrade
